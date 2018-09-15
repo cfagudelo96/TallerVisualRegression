6d8b06b4cf6666c85b1a194b7a1870dd9f685f50
Carlos Felipe Agudelo Ospina <cf.agudelo96@gmail.com>
2018-09-15 16:25:25 -0500

    Página de colores completada

--- colors.js
+++ colors.js
@@ -11,14 +11,14 @@
  * @param   Number  b       The blue color value
  * @return  Array           The HSL representation
  */
-function rgbToHsl(r, g, b){
+function rgbToHsl(r, g, b) {
     r /= 255, g /= 255, b /= 255;
     var max = Math.max(r, g, b), min = Math.min(r, g, b);
     var h, s, l = (max + min) / 2;
 
-    if(max == min){
+    if(max == min) {
         h = s = 0; // achromatic
-    }else{
+    } else {
         var d = max - min;
         s = l > 0.5 ? d / (2 - max - min) : d / (max + min);
         switch(max){
@@ -28,7 +28,6 @@ function rgbToHsl(r, g, b){
         }
         h /= 6;
     }
-
     return [h, s, l];
 }
 
@@ -43,13 +42,12 @@ function rgbToHsl(r, g, b){
  * @param   Number  l       The lightness
  * @return  Array           The RGB representation
  */
-function hslToRgb(h, s, l){
+function hslToRgb(h, s, l) {
     var r, g, b;
-
-    if(s == 0){
+    if(s == 0) {
         r = g = b = l; // achromatic
-    }else{
-        function hue2rgb(p, q, t){
+    } else {
+        function hue2rgb(p, q, t) {
             if(t < 0) t += 1;
             if(t > 1) t -= 1;
             if(t < 1/6) return p + (q - p) * 6 * t;
@@ -57,15 +55,13 @@ function hslToRgb(h, s, l){
             if(t < 2/3) return p + (q - p) * (2/3 - t) * 6;
             return p;
         }
-
         var q = l < 0.5 ? l * (1 + s) : l + s - l * s;
         var p = 2 * l - q;
         r = hue2rgb(p, q, h + 1/3);
         g = hue2rgb(p, q, h);
         b = hue2rgb(p, q, h - 1/3);
     }
-
-    return [r * 255, g * 255, b * 255];
+    return [Math.floor(r * 255), Math.floor(g * 255), Math.floor(b * 255)];
 }
 
 /**
@@ -79,7 +75,7 @@ function hslToRgb(h, s, l){
  * @param   Number  b       The blue color value
  * @return  Array           The HSV representation
  */
-function rgbToHsv(r, g, b){
+function rgbToHsv(r, g, b) {
     r = r/255, g = g/255, b = b/255;
     var max = Math.max(r, g, b), min = Math.min(r, g, b);
     var h, s, v = max;
@@ -87,17 +83,16 @@ function rgbToHsv(r, g, b){
     var d = max - min;
     s = max == 0 ? 0 : d / max;
 
-    if(max == min){
+    if(max == min) {
         h = 0; // achromatic
-    }else{
-        switch(max){
+    } else {
+        switch(max) {
             case r: h = (g - b) / d + (g < b ? 6 : 0); break;
             case g: h = (b - r) / d + 2; break;
             case b: h = (r - g) / d + 4; break;
         }
         h /= 6;
     }
-
     return [h, s, v];
 }
 
@@ -112,7 +107,7 @@ function rgbToHsv(r, g, b){
  * @param   Number  v       The value
  * @return  Array           The RGB representation
  */
-function hsvToRgb(h, s, v){
+function hsvToRgb(h, s, v) {
     var r, g, b;
 
     var i = Math.floor(h * 6);
@@ -120,8 +115,7 @@ function hsvToRgb(h, s, v){
     var p = v * (1 - s);
     var q = v * (1 - f * s);
     var t = v * (1 - (1 - f) * s);
-
-    switch(i % 6){
+    switch(i % 6) {
         case 0: r = v, g = t, b = p; break;
         case 1: r = q, g = v, b = p; break;
         case 2: r = p, g = v, b = t; break;
@@ -129,7 +123,6 @@ function hsvToRgb(h, s, v){
         case 4: r = t, g = p, b = v; break;
         case 5: r = v, g = p, b = q; break;
     }
-
-    return [r * 255, g * 255, b * 255];
+    return [Math.floor(r * 255), Math.floor(g * 255), Math.floor(b * 255)];
 }
 	
--- script.js
+++ script.js
@@ -1,9 +1,54 @@
-function randomPalette(){
+const R = 0;
+const G = 1;
+const B = 2;
+const H = 0;
+const S = 1;
+const L = 2;
+const CSS_TO_REPLACE = [
+    '.website-background{ color: ',
+    '.element-text{ color: ',
+    '.element-border{ border-color: ',
+    '.element-background{ background-color: ',
+    '.header{ color: '
+];
+const DIVISIONS = 5;
 
+function randomPalette() {
+    var increment = 360 / DIVISIONS;
+    var saturation = Math.random();
+    var lightness = Math.random();
+    var hue = randomNumberBetween(0, 359);
+    var colors = [];
+    for (var i = 0; i < DIVISIONS; i++) {
+        colors.push(hslToRgb(hue / 360, saturation, lightness));
+        hue = (hue + increment) % 360;
+    }
+    generateRules(colors);
 }
 
+function generateRules(colors) {
+    let cssText = '';
+    for (var i = 0; i < colors.length; i++) {
+        rgbCssRule = rgbToCssRule(colors[i]);
+        cssText = cssText + `${CSS_TO_REPLACE[i]}${rgbCssRule}; }\n`;
+        document.getElementById(`color${i + 1}`).style.background = rgbCssRule;
+    }
+    document.getElementById('css-rules').value = cssText;
+}
 
+function rgbToCssRule(rgb) {
+    return `rgb(${rgb[R]}, ${rgb[G]}, ${rgb[B]})`;
+}
 
-function generateRules(){
+function randomNumberBetween(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
 
+function cleanPalette() {
+    let cssText = '';
+    for (var i = 0; i < DIVISIONS; i++) {
+        cssText = cssText + `${CSS_TO_REPLACE[i]}#FFFFFF; }\n`;
+        document.getElementById(`color${i + 1}`).style.background = '#FFFFFF';
+    }
+    document.getElementById('css-rules').value = cssText;
 }
