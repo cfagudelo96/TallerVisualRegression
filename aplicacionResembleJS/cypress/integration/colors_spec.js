488ff30045358d6606b0b20150f489cf1f87cd53
Carlos Felipe Agudelo Ospina <cf.agudelo96@gmail.com>
2018-09-18 23:34:18 -0500

    Avance

--- aplicacionResembleJS/cypress/integration/colors_spec.js
+++ aplicacionResembleJS/cypress/integration/colors_spec.js
@@ -1,12 +1,11 @@
 describe('Generador de paleta de colores', function() {
     it('Visita la página de generación de colores y genera 2 paletas', function() {
-        const date = new Date();
         cy.visit('https://cfagudelo96.github.io/');
         cy.contains('Generar nueva paleta').click();
         cy.wait(500);
-        cy.screenshot('generador-' + date.getTime() + '-pre');
+        cy.screenshot('generador-' + Cypress.env('id') + '-pre');
         cy.contains('Generar nueva paleta').click();
         cy.wait(500);
-        cy.screenshot('generador-' + date.getTime() + '-post');
+        cy.screenshot('generador-' + Cypress.env('id') + '-post');
     });
 });
